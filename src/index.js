1978d53fb84ae73982e3130e136705596b9c2043
ensomarket <market.enso@gmail.com>
2025-11-28 19:51:16 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+// ESTA LÍNEA ES LA CLAVE: Importa el archivo de estilos de Tailwind
+import './index.css';
 
 // Usamos el ID 'root' definido en public/index.html
 const container = document.getElementById('root');
